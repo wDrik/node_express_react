bd1cd1d679e02402787ba0b3f3d680a911c76406
Iorgen Wildrik <w.drik@outlook.com>
2018-06-09 13:32:37 -0300

    Main component

--- front/src/components/MainComponent.js
+++ front/src/components/MainComponent.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 class MainComponent extends Component {
     render() {
         return(
-            <h1>MainComponent</h1>
+            <Container>
+                <Row>
+                    <Col xs="12" sm="12" md="12" className="text-center m-1tbem">
+                        <h1>Fintch</h1>
+                        <h4>Simple financial system made with node.js and React</h4>
+                        <Link className="btn btn-primary m-1lrem" to="/category">See Categories</Link>
+                        <Link className="btn btn-primary" to="/bill">See Bills</Link>
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
